6590ea0935ad2be0b3b487c1d3d20d1391d9ee1c
Vibhuti-Shrivastava <vibhutishri23@gmail.com>
2024-04-27 20:35:41 +0530

    testing and other updates for VendorModule

---
+++ ecommerce/src/com/test/VendorServiceTest.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import com.exception.ResourceNotFoundException;
+import com.service.VendorService;
+
+public class VendorServiceTest {
+	VendorService vendorService=new VendorService();
+
+//2
+	@Test
+	public void deleteByid() {
+        // Use case 1: Invalid ID
+        int id = 100; 
+        String expectedErrorMessage = "Id given is Invalid!!";
+        try {
+            vendorService.deleteByid(id);
+            
+        } catch (SQLException e) {
+        	 fail(e.getMessage());
+        } catch (ResourceNotFoundException e) {
+        	 
+            assertEquals(expectedErrorMessage, e.getMessage());
+        }
+   
+    }
+
+
+}
